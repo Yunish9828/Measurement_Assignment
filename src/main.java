53ebddd04a214123e8d49e4ac8b0624679621d54
[yunistha2019@gmail.com] <yunistha2019@gmail.com>
2024-08-20 22:15:48 +0545

    Measurement

--- src/main.java
+++ src/main.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class main {
+import models.Measurement;
+
+public class main {
+    public static void main(String[] args) {
+        Measurement meas = new Measurement(100);
+        System.out.println(meas.getcm()+ "cm");
+        System.out.println(meas.getKm()+ "Km");
+        System.out.println(meas.getMm()+ "Mm");
+        System.out.println(meas.getDm()+ "Dm");
+
+    }
 }
